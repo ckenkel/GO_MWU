2f698f07a0d0a402e3986c3b91f9e0669ae6ac70
ckenkel <ckenkel@usc.edu>
2019-07-09 13:19:52 -0700

    Add files via upload

--- GO_MWU.R
+++ GO_MWU.R
@@ -18,10 +18,10 @@
 
 
 # Edit these to match your data file names: 
-input="heats.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
-goAnnotations="amil_defog_iso2go.tab" # two-column, tab-delimited, one line per gene, multiple GO terms separated by semicolon. If you have multiple lines per gene, use nrify_GOtable.pl prior to running this script.
+input="GOpatchHost.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
+goAnnotations="plob_iso2go.tab" # two-column, tab-delimited, one line per gene, multiple GO terms separated by semicolon. If you have multiple lines per gene, use nrify_GOtable.pl prior to running this script.
 goDatabase="go.obo" # download from http://www.geneontology.org/GO.downloads.ontology.shtml
-goDivision="MF" # either MF, or BP, or CC
+goDivision="CC" # either MF, or BP, or CC
 source("gomwu.functions.R")
 
 
@@ -42,17 +42,85 @@ gomwuStats(input, goDatabase, goAnnotations, goDivision,
 quartz()
 results=gomwuPlot(input,goAnnotations,goDivision,
 #	absValue=-log(0.05,10),  # genes with the measure value exceeding this will be counted as "good genes". Specify absValue=0.001 if you are doing Fisher's exact test for standard GO enrichment or analyzing a WGCNA module (all non-zero genes = "good genes").
-	absValue=1,
-	level1=0.1, # FDR threshold for plotting. Specify level1=1 to plot all GO categories containing genes exceeding the absValue.
-	level2=0.05, # FDR cutoff to print in regular (not italic) font.
-	level3=0.01, # FDR cutoff to print in large bold font.
+	absValue=-log(0.05,10), 
+	level1=0.05, # FDR threshold for plotting. Specify level1=1 to plot all GO categories containing genes exceeding the absValue.
+	level2=0.01, # FDR cutoff to print in regular (not italic) font.
+	level3=0.005, # FDR cutoff to print in large bold font.
 	txtsize=1.2,    # decrease to fit more on one page, or increase (after rescaling the plot so the tree fits the text) for better "word cloud" effect
 	treeHeight=0.5, # height of the hierarchical clustering tree
-#	colors=c("dodgerblue2","firebrick1","skyblue","lightcoral") # these are default colors, un-remar and change if needed
+	colors=c("dodgerblue2","firebrick1","skyblue2","lightcoral") # these are default colors, un-remar and change if needed "dodgerblue2","firebrick1","skyblue2","lightcoral"
 )
 # manually rescale the plot so the tree matches the text 
 # if there are too many categories displayed, try make it more stringent with level1=0.05,level2=0.01,level3=0.001.  
 
 # text representation of results, with actual adjusted p-values
-results
+results[order(results$pval),]
 
+################################
+####### Heatmaps of 'good genes'
+################################
+
+#Note: can run this script anytime after desired GO MWU has been run
+
+library(pheatmap)
+library(RColorBrewer)
+
+gg=read.table("plob_iso2gene.tab",header=F,sep="	",quote="",row.names=NULL,stringsAsFactors=FALSE)  #the iso2gene file for your species 
+dfull=read.csv("hostVSDandPVALS_no_g4_deseq1_4jun_plusPC1.csv") 
+rownames(dfull)<-dfull$X #make gene names rownames
+edata=c(2:15) #only columns with your expression data 
+d=dfull[dfull$pval.p<=0.05 & !is.na(dfull$pval.p),]
+
+golist=read.table("CC_GOpatchHost.csv",sep="	",header=T) #read in proper GO list from gomwu output - BP/MF/CC
+d=read.csv("VSDs_GObinaryHostPatch.csv") #read in VSD file for sig DE genes
+
+
+
+in.mwu=paste("MWU",goDivision,input,sep="_")
+pv=read.table(in.mwu,header=T)
+
+#######################
+
+gene=subset(pv,name=="mitochondrial protein complex") #write GO term of interest here from your sig list#
+t=gene[,5]
+t
+
+is=golist$seq[grep(t,golist$term,ignore.case=T)]
+length(is) #should match denominator in MWU dendrogram plot for term
+
+#####################loop through genes matching GO term in module or in "good expression" subset
+
+sel=c();gnms=c()
+for ( i in is){
+	if (i %in% d$X){
+		sel=rbind(sel,d[d$X==i,])
+		gnms=append(gnms,substr(paste(as.character(gg$V2[gg$V1==i]),collapse="."),1,50))
+	}
+}
+row.names(sel)=paste(gnms,sel$X,sep=".")
+nrow(sel)  
+rownames(sel)
+
+
+exp=sel[,edata]
+if (length(exp[,1])==1) { exp=rbind(exp,exp) }
+nrow(exp)
+#should match 'good genes' numerator in figure
+rownames(exp)
+#rownames(daat)
+
+means=apply(exp,1,mean) # means of rows
+expc=exp-means #rescale expression data so it's up and down relative to mean
+
+#reorder columns to match healthy/wps
+expc<-expc[,c(1,3,5,7,9,11,13,2,4,6,8,10,12,14)] #host
+
+
+col=color = colorRampPalette(rev(brewer.pal(n = 7, name ="RdBu")),bias=0.55)(30)
+#lower bias number gives more blues; higher bias gives more reds
+#mess with bias to get white at 0
+
+#quartz()
+#pdf("HeatmapPatchUp.pdf",width=8,height=17)
+pheatmap(expc,color=col,cluster_cols=F,clustering_distance_rows="correlation") #plot the heatmap
+#dev.off()
